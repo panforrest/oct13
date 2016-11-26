9203b5607c655c0104f1dc938dde16b1191d355a
panforrest <guoqianp@gmail.com>
2016-11-25 21:14:04 -0500

    1st commit

---
+++ routes/twitter.js
@@ -0,0 +1,60 @@
+var express = require('express')
+var router = express.Router()
+var Twitter = require('twitter')
+
+
+router.get('/:action', function(req, res, next) {
+	var actions = ['timeline', 'search']
+	var action = req.params.action
+
+	if (actions.indexOf(action) == -1){
+		res.json({
+			confirmation:'fail',
+			message: 'Invalid action. Please select search or timeline.'
+		})
+
+		return
+	}
+
+	var client = new Twitter({
+	  consumer_key: '2fyOw1DABfCv9O7rCIXCL0NS9',
+	  consumer_secret: '7rLPuKm0IM8SS8SVUF8SMdkK8BGg1vA2bvc9eaDlHRHBAE0vxQ',
+	  access_token_key: '1118902316-AJXLU9qeaKhskrYCQH7MKGetxM3068mJgtV85IS',
+	  access_token_secret: 'KxgCxyupDTq3RTGksqHGH90Uh3tUDTzCJeaZLpZriOfd1'
+	})
+
+	var params = {}
+	var url = null
+	if (action == 'timeline'){
+		params['screen_name'] = req.query.username
+		url = 'statuses/user_timeline'
+	}
+	else if (action == 'search'){
+		params['q'] = req.query.term
+		url = 'search/tweets'
+	}
+
+	client.get(url, params, function(error, tweets, response) {
+	  if (error == null) {
+	    console.log(tweets)
+	  }
+
+	    var list = (tweets.statuses == null) ? tweets : tweets.statuses
+	    var title = (action == 'timeline') ? params['screen_name'] : params['q']
+		var content = {
+			title: title,
+			tweets: list
+		}
+
+		var format = (req.query.format == null) ? 'html' : req.query.format
+		if (format == 'json'){
+			res.json(tweets)
+		}
+		else {
+			res.render('twitter', content)
+		}
+	})
+})
+
+
+module.exports = router
---
+++ routes/yelp.js
@@ -0,0 +1,41 @@
+var express = require('express')
+var router = express.Router()
+var Yelp = require('yelp')
+
+
+router.get('/', function(req, res, next) {
+	var term = req.query.term
+	var location = req.query.location
+
+	var yelp = new Yelp({
+	  consumer_key: 'sMjsmliC9gKiXtvCXSQxCA',
+	  consumer_secret: 'teV_SLD48JWA2d2G3UQ7zuL6LdU',
+	  token: 'tFbXLcPFBDyb-Qq_TdQLibMiY0DLsaJl',
+	  token_secret: 'o0CdBGngWRHsGAq0DJ665OVRYvM',
+	})
+
+	yelp.search({ term:term, location:location })
+	.then(function (data) {
+	  console.log(data)
+		var content = {
+			title: term+' in '+location,
+			data: data
+		}
+
+		var format = (req.query.format == null) ? 'html' : req.query.format
+		if (format == 'json'){
+			res.json(data)
+		}
+		else {
+			res.render('yelp', content)
+		}
+
+	  return
+	})
+	.catch(function (err) {
+	  console.error(err)
+	})
+
+})
+
+module.exports = router
